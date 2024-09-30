3e9d0324870d1cdbe22fad4a4c53964548b31e56
Tim Denning <36028402+denningta@users.noreply.github.com>
2024-09-30 05:43:41 -0600

    fixed bug

--- tcp_server.py
+++ tcp_server.py
@@ -18,10 +18,10 @@ def start_server(host='0.0.0.0', port=65432):
                     if data:
                         print(f"Received data from {addr}: {data.decode()}")
 
-                        if (data == "T1"):
-                            conn.sendall("T1")
+                        if (data.decode() == "T1\n"):
+                            conn.sendall("T1\n")
                             time.sleep(5)
-                            conn.sendall("0.259,0.261,0.258,0.260,0.260")
+                            conn.sendall("0.259,0.261,0.258,0.260,0.260\n")
                         else:
                             # Send an immediate response
                             immediate_response = b"Immediate Response\n"
